c886d27ece695b8aa12ce307b355b634e04f6790
razielalmanza <razielmcr1@ciencias.unam.mx>
2019-11-13 01:55:35 -0600

    Se agregan algunas consultas

--- dml.sql
+++ dml.sql
@@ -1,15 +1,78 @@
 USE FBD2020_1_5312_TAREA_06;
 
 -- a. Encontrar el nombre y la ciudad de todos los empleados que trabajan en PEMEX.
+-- Sin probar.
+SELECT nombre,
+	   paterno,
+	   materno,
+	   empleados.ciudad
+FROM trabajar
+INNER JOIN empleados ON empleados.curp = trabajar.curp
+INNER JOIN empresas ON trabajar.rfc = empresas.rfc
+WHERE empresas.razon_social = 'PEMEX';
+
 -- b. Encontrar todos los empleados que no viven en la misma ciudad de la empresa en que trabajan.
+-- Sin probar.
+SELECT nombre,
+	   paterno,
+	   materno,
+	   empleados.ciudad
+FROM trabajar
+INNER JOIN empleados ON empleados.curp = trabajar.curp
+INNER JOIN empresas ON trabajar.rfc = empresas.rfc
+WHERE empleados.ciudad = empresas.ciudad;
+
 -- c. Calcular el salario mensual de todas las directoras.
+-- Poner F o M, genero
+-- Sin probar
+SELECT nombre,
+	   paterno,
+	   materno,
+	   salario_quincenal * 2
+FROM empresas
+INNER JOIN trabajar ON trabajar.rfc = empresas.rfc
+INNER JOIN empleados ON empresas.rfc = empleados.dirigir_empresa
+WHERE empleados.genero = 'F';
+
 -- d. Obtener la información de los directores (en general) y empresas que comenzaron a dirigir 
 --    durante el segundo y cuarto trimestre de 2018.
+-- Fechas invertidas
+-- Sin probar
+SELECT nombre,
+	   paterno,
+	   materno,
+	   razon_social
+FROM empresas
+INNER JOIN empleados ON empleados.dirigir_empresa = empresas.rfc
+WHERE (fecha_inicio < '06-30-2018' and fecha_inicio > '04-01-2018') or (fecha_inicio > '10-01-2018' and fecha_inicio < '12-31-2018') 
+
 -- e. Encontrar a todos los empleados que viven en la misma ciudad y en la misma calle que su supervisor.
--- f. Obtener una lista de cada compañía y el salario promedio que pag-- a. La información se debe
+-- PENDIENTE
+
+-- f. Obtener una lista de cada compañía y el salario promedio que paga. La información se debe
 --    mostrar por compañía, año, y género.
+-- Sin probar
+SELECT empresas.rfc,
+	   AVG(salario_quincenal) AS Salario
+FROM empresas
+INNER JOIN trabajar ON trabajar.rfc = empresas.razon_social
+GROUP BY empresas.rfc;
+
 -- g. Empleados que colaboran en proyectos que controlan empresas para las que no trabajan.
+-- PENDUENTE
+--SELECT empleados.curp, 
+--	   empleados.nombre, 
+--	   empleados.paterno, 
+--	   empleados.materno, 
+--	   proyectos.num_proyecto
+--FROM colaborar
+--INNER JOIN empleados ON empleados.curp = colaborar.curp
+--INNER JOIN proyectos ON proyectos.num_proyecto = colaborar.num_proyecto
+-- WHERE (empleados.curp, proyectos.rfc) NOT IN (SELECT curp,rfc FROM trabajar); 
+
+
 -- h. Encontrar el máximo, mínimo y total de salarios pagados por cada compañía.
+
 -- i. Encontrar información de los empleados y número de horas que dedican a los proyectos.
 --    Interesan aquellos empleados que colaboran en al menos dos proyectos y en donde el
 --    número de horas que dediquen a algún proyecto sea mayor a 20.
