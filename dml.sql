f0e4014b02fb9b09fb427e820d9bb17cacafcebd
razielalmanza <razielmcr1@ciencias.unam.mx>
2019-11-14 03:07:08 -0600

    Se agregan mas querys

--- dml.sql
+++ dml.sql
@@ -39,7 +39,6 @@ WHERE empleados.genero = 'F';
 
 -- d. Obtener la información de los directores (en general) y empresas que comenzaron a dirigir 
 --    durante el segundo y cuarto trimestre de 2018.
--- Fechas invertidas
 -- Sin probar
 SELECT nombre,
 	   paterno,
@@ -54,12 +53,12 @@ WHERE (fecha_inicio < '06-30-2018' and fecha_inicio > '04-01-2018') or (fecha_in
 
 -- f. Obtener una lista de cada compañía y el salario promedio que paga. La información se debe
 --    mostrar por compañía, año, y género.
--- Sin probar
-SELECT empresas.rfc,
+-- Falta lo de mostrar por compañia, etc
+SELECT empresas.razon_social,
 	   AVG(salario_quincenal) AS Salario
 FROM empresas
-INNER JOIN trabajar ON trabajar.rfc = empresas.razon_social
-GROUP BY empresas.rfc;
+INNER JOIN trabajar ON trabajar.rfc = empresas.rfc
+GROUP BY empresas.razon_social;
 
 -- g. Empleados que colaboran en proyectos que controlan empresas para las que no trabajan.
 -- PENDUENTE
@@ -75,15 +74,71 @@ GROUP BY empresas.rfc;
 
 
 -- h. Encontrar el máximo, mínimo y total de salarios pagados por cada compañía.
+-- Es total count o sum? 
+SELECT empresas.razon_social,
+	   MAX(salario_quincenal) AS Maximo,
+	   MIN(salario_quincenal) AS Minimo,
+	   COUNT(salario_quincenal) AS Total
+FROM empresas
+INNER JOIN trabajar ON trabajar.rfc = empresas.rfc
+GROUP BY empresas.razon_social;
 
 -- i. Encontrar información de los empleados y número de horas que dedican a los proyectos.
 --    Interesan aquellos empleados que colaboran en al menos dos proyectos y en donde el
 --    número de horas que dediquen a algún proyecto sea mayor a 20.
 -- j. Encontrar la cantidad de empleados en cada compañía, por año, trimestre y género.
 -- k. Encontrar el nombre del empleado que gana más dinero en cada compañía.
+SELECT empresas.razon_social,
+	   nombre,
+	   MAX(salario_quincenal) AS MejorPagado
+FROM empresas
+INNER JOIN trabajar ON trabajar.rfc = empresas.rfc
+INNER JOIN empleados ON empleados.curp = trabajar.curp
+GROUP BY empresas.razon_social,nombre;
+
 -- l. Obtener una lista de los empleados que ganan más del salario promedio que pagan las
 -- compañías.
+
+SELECT nombre,
+	   paterno,
+	   Salario,
+	   PromedioSalario
+-- Tabla con los salarios de todos los empleados
+FROM
+	(SELECT nombre,
+			paterno,
+		   salario_quincenal AS Salario,
+		   empresas.rfc AS rfc
+	FROM empresas
+	INNER JOIN trabajar ON trabajar.rfc = empresas.rfc
+	INNER JOIN empleados ON empleados.curp = trabajar.curp) AS a 
+-- Join con la tabla con los promedios de salarios por compania
+INNER JOIN (
+	SELECT empresas.rfc as rfc,
+		   AVG(salario_quincenal) AS PromedioSalario
+	FROM empresas
+	INNER JOIN trabajar ON trabajar.rfc = empresas.rfc
+	GROUP BY empresas.rfc) AS b
+ON a.rfc = b.rfc
+WHERE Salario > PromedioSalario;
+
 -- m. Encontrar la compañía que tiene menos empleados y listar toda la información de los mismos.
+-- Hay varias empresas con un empleado.
+SELECT min_num_empleados,
+	a.*
+FROM (SELECT rfc, COUNT(curp) AS num_empleados
+	FROM trabajar
+	GROUP BY rfc) e 
+	INNER JOIN 
+	(SELECT MIN(n.num_empleados) AS min_num_empleados
+	FROM (SELECT rfc, COUNT(curp) AS num_empleados
+		FROM trabajar
+		GROUP BY rfc) AS n) m 
+	ON e.num_empleados = m.min_num_empleados
+	INNER JOIN trabajar t ON t.rfc = e.rfc
+	INNER JOIN empleados a ON a.curp = t.curp;
+
+
 -- n. Información de los proyectos en los que colaboran los empleados que son directores.
 -- o. Encontrar la compañía que tiene empleados en cada una de las ciudades que hayas  definido.
 -- p. Empleados que dejaron de colaborar en proyectos, antes de la fecha de finalización de los mismos.
@@ -97,4 +152,3 @@ GROUP BY empresas.rfc;
 -- x. Número de empleados que colaboran en los proyectos que controla cada empresa para aquellos proyectos que hayan iniciado en diciembre.
 -- y. Crea una vista con la información de los empleados y compañías en que trabajan, de aquellos 
 --    empleados que lo hagan en al menos tres compañías diferentes.
--- 5. Indica para la política de mantenimiento de llaves foráneas, qué ventajas
